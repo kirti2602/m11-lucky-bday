e9617c122fbeb8c58c2c1d7fbc379d516332569b
Kirti Sharma <kirti2001sharma@gmail.com>
2021-09-19 00:50:27 +0530

    almost done

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 let bday = document.getElementById("bday");
 let luckyNo = document.getElementById("luckyNo");
 let button = document.getElementById("btn");
+let msg = document.getElementById("luckyMsg");
 
-let bdayString = bday.value;
-let newString = bdayString.replaceAll('-','');
-
-
-
+// let replacedString = bday.value.replaceAll('-','');
 
 button.addEventListener('click', checkLucky);
 
 function checkLucky(){
-    console.log(bdayString)
-    console.log(newString)
+    let sum = 0;
+    for(let i = 0; i< bday.value.replaceAll('-','').length; i++){
+        let num = Number(bday.value.replaceAll('-','')[i]);
+        sum+= num;
+    }
+    
+    if(bday.value === '' || luckyNo.value === ''){
+        msg.innerHTML = "please enter both the fields to proceed 🚫";
+    }
+
+    else if(sum % luckyNo.value === 0){
+        msg.innerHTML = "yayy! your birthday is lucky 🥳"
+        msg.style.color = "green"
+    }
+
+    else{
+        msg.innerHTML = "not lucky this time 😢";
+        msg.style.color = "red";
+
+    }
 }
+
